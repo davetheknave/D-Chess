7940592040564cade253373758038765bde3793b
davetheknave <knavedavid@gmail.com>
2018-07-19 14:05:43 -0400

    Add Alice Rules

--- Pieces/Piece.gd
+++ Pieces/Piece.gd
@@ -6,6 +6,7 @@ export (NodePath) var shape
 var position = null
 var white = true
 var alice = false
+var selected = false
 var board
 
 func get_name():
@@ -24,15 +25,36 @@ func set_material(material):
 	if shape != null:
 		get_node(shape).set_surface_material(0, material)
 
+func update_material():
+	if white:
+		if alice:
+			if selected:
+				set_material(load("res://Pieces/WhiteAliceHighlight.tres"))
+			else:
+				set_material(load("res://Pieces/WhiteAlice.tres"))
+		else:
+			if selected:
+				set_material(load("res://Pieces/WhiteHighlight.tres"))
+			else:
+				set_material(load("res://Pieces/White.tres"))
+	else:
+		if alice:
+			if selected:
+				set_material(load("res://Pieces/BlackAliceHighlight.tres"))
+			else:
+				set_material(load("res://Pieces/BlackAlice.tres"))
+		else:
+			if selected:
+				set_material(load("res://Pieces/BlackHighlight.tres"))
+			else:
+				set_material(load("res://Pieces/Black.tres"))
+
 func is_white():
 	return white
 
 func set_white(isWhite):
 	white = isWhite
-	if isWhite:
-		set_material(load("res://Pieces/White.tres"))
-	else:
-		set_material(load("res://Pieces/Black.tres"))
+	update_material()
 
 func get_material():
 	if shape != null:
@@ -41,37 +63,34 @@ func get_material():
 func get_moves(board):
 	return null
 
-func alice(isAlice):
-	pass
+func alice():
+	alice = !alice
+	update_material()
 
 func select():
-	if white:
-		set_material(load("res://Pieces/WhiteHighlight.tres"))
-	else:
-		set_material(load("res://Pieces/BlackHighlight.tres"))
+	selected = true
+	update_material()
 
 func deselect():
-	if white:
-		set_material(load("res://Pieces/White.tres"))
-	else:
-		set_material(load("res://Pieces/Black.tres"))
+	selected = false
+	update_material()
 
 # Appends a move if it is possible, and returns true if no more moves can be made in this direction
 func append_move(list, x, y):
-	if !board.bounds(x): return true
-	if !board.bounds(y): return true
+	if !board.bounds(x) or !board.bounds(y): return true
 	var piece = board.get_piece_at(x, y)
 	if piece != null:
-		if piece.white and white:
+		if self.alice != piece.alice:
+			return false
+		if self.white == piece.white:
 			return true
-		elif !piece.white and !white:
-			return true
-	list.append([x, y])
-	if piece!= null:
+		list.append([x, y])
 		return true
+	list.append([x, y])
 	return false
 
 func move(x, y):
 	position[0] = x
 	position[1] = y
+	alice()
 	board.enPassant = [-1, -1]
